bb91e429171e9c3451f9fa6e102f3bdd49d80406
Fu-Lin Peng <fulinpeng53@gmail.com>
2024-07-25 20:28:15 -0400

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "John";
+            int age = 31;
+            char middleInitial = 'D';
+            bool isOver18 = true;
+            double currentTemp = 77.9;
+            decimal currentPrice = 29.99m;
+
+            Console.WriteLine($"Name: {name}\n" +
+                              $"Age: {age}\n" +
+                              $"Middle Initial: {middleInitial}\n" +
+                              $"{name} is over 18: {isOver18}\n" +
+                              $"Current Temperature: {currentTemp}\n" +
+                              $"Current Price: {currentPrice}");
         }
     }
 }
